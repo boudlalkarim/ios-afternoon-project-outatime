960c55fbbd238bfa6ffcd77bd1f4f71f62c0b499
Karim Boudlal <karim_boudlal@yahoo.com>
2019-10-08 23:10:59 -0400

    started

---
+++ outatime/TimeCircuitsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TimeCircuitsViewController.swift
+//  outatime
+//
+//  Created by Karim Boudlal on 10/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ outatime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  outatime
+//
+//  Created by Karim Boudlal on 10/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    class TimeCircuitsViewController: UITableView {
+        
+        
+    }
+    class DatePickerViewController: UITableView {
+        
+    }
+}
+
